2f95d66786f4fb1ef7840db9895ce58bb9e343bb
Yuu <123919860+TN412@users.noreply.github.com>
2025-03-20 10:23:46 +0700

    Add files via upload

---
+++ bmttnc-hutech-2280602069/bmttnc-hutech-2280602069/Lab01/ex02/ex02_01.py
@@ -0,0 +1,5 @@
+#nhập tên và tuổi từ người dùng 
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+# in ra thông diệp chào mừng với thông tin đã nhập
+print("Chào mừng ", ten, " bạn ,", tuoi, " tuổi.")
