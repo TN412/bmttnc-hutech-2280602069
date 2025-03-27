d8f986da850ff151b10ffe567f35cf4d2d9b9005
Yuu <123919860+TN412@users.noreply.github.com>
2025-03-27 10:33:03 +0700

    Delete bmttnc-hutech-2280602069 directory

--- bmttnc-hutech-2280602069/Lab01/ex02/ex02_03.py
+++
@@ -1,7 +0,0 @@
-#nhập số từ người dùng
-so= int(input("Nhập số nguyên: "))
-#kiểm tra số đó là số chẵn hay không
-if so % 2 == 0:
-    print(so, "la so chan")
-else:
-    print(so, "khong phai la so chan") 
