8f01f770ef325bda0243e44849810108d01e7923
Yuu <123919860+TN412@users.noreply.github.com>
2025-03-27 10:30:42 +0700

    Delete bmttnc-hutech-2280602069 directory

--- bmttnc-hutech-2280602069/Lab01/ex02/ex02_10.py
+++
@@ -1,5 +0,0 @@
-def dao_nguoc_chuoi(chuoi):
-    return chuoi[::-1]
-#sử dụng hàm và in ra kết quả
-input_string = input("Nhập chuỗi cần đảo ngược: ")
-print("Chuỗi sau khi đảo ngược là: ", dao_nguoc_chuoi(input_string))
